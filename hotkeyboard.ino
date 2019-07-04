66815a69857ace9036a4297ece7ccfd662c306aa
Bryce <30963872+fwacer@users.noreply.github.com>
2019-07-03 22:24:47 -0600

    Changed from the Keyboard library to HID-Project
    Switched libraries to allow use of media volume keys and num lock.
    If the user accidentally switched off the numlock key, the ALT keycodes for the greek letters would no longer work. So, I just always force it to be on.

--- hotkeyboard.ino
+++ hotkeyboard.ino
@@ -1,6 +1,7 @@
-#include <Keyboard.h>
+//#include <Keyboard.h>
+#include "HID-Project.h"
 #include <rickroll.h>
-#include <U8glib.h> // lcd display
+//#include <U8glib.h> // lcd display
 
 // Define Pins
 #define SHIFT_DATA   6 //Shift register 
@@ -13,27 +14,10 @@
 #define INPUT_FIRST 10
 #define INPUT_LAST A3
 
-#define TONE_PIN 9
+#define PIN_TONE 9
+//#define PIN_NUMLOCK_LED 1
 
-// Define aliases for keycodes
-#define KP_1  225
-#define KP_2  226
-#define KP_3  227
-#define KP_4  228
-#define KP_5  229
-#define KP_6  230
-#define KP_7  231
-#define KP_8  232
-#define KP_9  233
-#define KP_0  234
-#define KP_PERIOD 235
-
-#define KEY_NUMLOCK 53+136 //83 + 136
-#define KEY_MUTE 263 // Keyboard Mute
-#define KEY_VOLUMEUP 264 // Keyboard Volume Up
-#define KEY_VOLUMEDOWN 265 // Keyboard Volume Down
-
-void printKeyPressed(int input_registered, int current_output){
+void keyPressed(int input_registered, int current_output){
   //Serial.println("I"+String(input_registered)+":O"+String(current_output));
 
   /*
@@ -53,81 +37,81 @@ void printKeyPressed(int input_registered, int current_output){
     case 0:
     switch(current_output){
       case 1: // Greek letter Phi
-      Serial.println("F7");
-      Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.write(KP_9);
-      Keyboard.write(KP_6);
-      Keyboard.write(KP_6);   
+      // Serial.println("F7");
+      BootKeyboard.press(KEY_LEFT_ALT);
+      BootKeyboard.write(KEYPAD_9);
+      BootKeyboard.write(KEYPAD_6);
+      BootKeyboard.write(KEYPAD_6);   
       break;
       case 2: // Greek letter Tau
-      Serial.println("F8");
-      Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.write(KP_9);
-      Keyboard.write(KP_6);
-      Keyboard.write(KP_4);   
+      // Serial.println("F8");
+      BootKeyboard.press(KEY_LEFT_ALT);
+      BootKeyboard.write(KEYPAD_9);
+      BootKeyboard.write(KEYPAD_6);
+      BootKeyboard.write(KEYPAD_4);   
       break;
       case 5: // Greek letter Sigma
-      Serial.println("E8");
-      Keyboard.write(KP_9);
-      Keyboard.write(KP_6);
-      Keyboard.write(KP_3);   
+      // Serial.println("E8");
+      BootKeyboard.write(KEYPAD_9);
+      BootKeyboard.write(KEYPAD_6);
+      BootKeyboard.write(KEYPAD_3);   
       break;
       case 6: // Greek letter Theta
-      Serial.println("E7");
-      Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.write(KP_9);
-      Keyboard.write(KP_5);
-      Keyboard.write(KP_2);   
+      // Serial.println("E7");
+      BootKeyboard.press(KEY_LEFT_ALT);
+      BootKeyboard.write(KEYPAD_9);
+      BootKeyboard.write(KEYPAD_5);
+      BootKeyboard.write(KEYPAD_2);   
       break;
       case 7: // Greek letter Pi
-      Serial.println("E6");
-      Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.write(KP_9);
-      Keyboard.write(KP_6);
-      Keyboard.write(KP_0);   
+      // Serial.println("E6");
+      BootKeyboard.press(KEY_LEFT_ALT);
+      BootKeyboard.write(KEYPAD_9);
+      BootKeyboard.write(KEYPAD_6);
+      BootKeyboard.write(KEYPAD_0);   
       break;
       case 8: // Greek letter Mu
-      Serial.println("E5");
-      Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.write(KP_9);
-      Keyboard.write(KP_5);
-      Keyboard.write(KP_6);   
+      // Serial.println("E5");
+      BootKeyboard.press(KEY_LEFT_ALT);
+      BootKeyboard.write(KEYPAD_9);
+      BootKeyboard.write(KEYPAD_5);
+      BootKeyboard.write(KEYPAD_6);   
       break;
       case 9: // Greek letter capital Delta
-      Serial.println("E3");
-      Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.write(KP_9);
-      Keyboard.write(KP_1);
-      Keyboard.write(KP_6);  
+      // Serial.println("E3");
+      BootKeyboard.press(KEY_LEFT_ALT);
+      BootKeyboard.write(KEYPAD_9);
+      BootKeyboard.write(KEYPAD_1);
+      BootKeyboard.write(KEYPAD_6);  
       break;
       case 10: // Greek letter Gamma
-      Serial.println("E4");
-      Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.write(KP_9);
-      Keyboard.write(KP_4);
-      Keyboard.write(KP_7);   
+      // Serial.println("E4");
+      BootKeyboard.press(KEY_LEFT_ALT);
+      BootKeyboard.write(KEYPAD_9);
+      BootKeyboard.write(KEYPAD_4);
+      BootKeyboard.write(KEYPAD_7);   
       break;
     }
     break;
     case 4:
     switch(current_output){
       case 2: // Play a song!
-      Serial.println("G2");
-      playRickRoll(TONE_PIN);
+      // Serial.println("G2");
+      BootKeyboard.write(KEY_NUM_LOCK);
       break;
       case 5: // Greek letter capital Sigma (sum)
-      Serial.println("G7");
-      Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.write(KP_2);
-      Keyboard.write(KP_2);
-      Keyboard.write(KP_8); 
+      // Serial.println("G7");
+      BootKeyboard.press(KEY_LEFT_ALT);
+      BootKeyboard.write(KEYPAD_2);
+      BootKeyboard.write(KEYPAD_2);
+      BootKeyboard.write(KEYPAD_8); 
       break;
       case 6: // Greek letter Omega
-      Serial.println("G6");
-      Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.write(KP_9);
-      Keyboard.write(KP_6);
-      Keyboard.write(KP_9); 
+      // Serial.println("G6");
+      BootKeyboard.press(KEY_LEFT_ALT);
+      BootKeyboard.write(KEYPAD_9);
+      BootKeyboard.write(KEYPAD_6);
+      BootKeyboard.write(KEYPAD_9); 
       /*
       Keyboard.press(KEY_LEFT_GUI);
       Keyboard.press('r');
@@ -145,99 +129,100 @@ void printKeyPressed(int input_registered, int current_output){
       */
       break;
       case 7: // Greek letter Epsilon
-      Serial.println("G5"); 
-      Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.write(KP_2);
-      Keyboard.write(KP_3);
-      Keyboard.write(KP_8); 
+      // Serial.println("G5"); 
+      BootKeyboard.press(KEY_LEFT_ALT);
+      BootKeyboard.write(KEYPAD_2);
+      BootKeyboard.write(KEYPAD_3);
+      BootKeyboard.write(KEYPAD_8); 
       break;
       case 8: // Greek letter Del (partial differential)
-      Serial.println("G4");
-      Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.write(KP_8);
-      Keyboard.write(KP_7);
-      Keyboard.write(KP_0); 
-      Keyboard.write(KP_6); 
+      // Serial.println("G4");
+      BootKeyboard.press(KEY_LEFT_ALT);
+      BootKeyboard.write(KEYPAD_8);
+      BootKeyboard.write(KEYPAD_7);
+      BootKeyboard.write(KEYPAD_0); 
+      BootKeyboard.write(KEYPAD_6); 
       break;
     }
     break;
     case 5:
     switch(current_output){
       case 1: // Find
-      Serial.println("A2");
-      Keyboard.press(KEY_LEFT_CTRL);
-      Keyboard.press('f');
+      // Serial.println("A2");
+      BootKeyboard.press(KEY_LEFT_CTRL);
+      BootKeyboard.press('f');
       break;
       case 2: // Open Explorer
-      Serial.println("A3");
-      Keyboard.press(KEY_LEFT_GUI);
-      Keyboard.press('e');
+      // Serial.println("A3");
+      BootKeyboard.press(KEY_LEFT_GUI);
+      BootKeyboard.press('e');
       break;
       case 5:
-      Serial.println("C3");
+      // Serial.println("C3");
       break;
       case 6:
-      Serial.println("A8");
+      // Serial.println("A8");
       break;
       case 7: // Begin recording with Windows game bar
-      Serial.println("A7");
-      Keyboard.press(KEY_LEFT_GUI);
-      Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.press('r');
+      // Serial.println("A7");
+      BootKeyboard.press(KEY_LEFT_GUI);
+      BootKeyboard.press(KEY_LEFT_ALT);
+      BootKeyboard.press('r');
       break;
       case 8: // Record the last 30 seconds with Windows game bar
-      Serial.println("A6");
-      Keyboard.press(KEY_LEFT_GUI);
-      Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.press('g');
+      // Serial.println("A6");
+      BootKeyboard.press(KEY_LEFT_GUI);
+      BootKeyboard.press(KEY_LEFT_ALT);
+      BootKeyboard.press('g');
       break;
       case 9: // Open settings
-      Serial.println("A4");
-      Keyboard.press(KEY_LEFT_GUI);
-      Keyboard.press('i');
+      // Serial.println("A4");
+      BootKeyboard.press(KEY_LEFT_GUI);
+      BootKeyboard.press('i');
       break;
       case 10:
-      Serial.println("A5");
+      // Serial.println("A5");
       break;
     }
     break;
     case 6:
     switch(current_output){
       case 5: // Greek letter Rho
-      Serial.println("F6");
-      Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.write(KP_9);
-      Keyboard.write(KP_6);
-      Keyboard.write(KP_1);   
+      // Serial.println("F6");
+      BootKeyboard.press(KEY_LEFT_ALT);
+      BootKeyboard.write(KEYPAD_9);
+      BootKeyboard.write(KEYPAD_6);
+      BootKeyboard.write(KEYPAD_1);   
       break;
       case 6: // Greek letter Nu
-      Serial.println("F5");
-      Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.write(KP_9);
-      Keyboard.write(KP_5);
-      Keyboard.write(KP_7);   
+      // Serial.println("F5");
+      BootKeyboard.press(KEY_LEFT_ALT);
+      BootKeyboard.write(KEYPAD_9);
+      BootKeyboard.write(KEYPAD_5);
+      BootKeyboard.write(KEYPAD_7);   
       break;
       case 7: // Greek letter Lambda
-      Serial.println("F4");
-      Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.write(KP_9);
-      Keyboard.write(KP_5);
-      Keyboard.write(KP_5);   
+      // Serial.println("F4");
+      BootKeyboard.press(KEY_LEFT_ALT);
+      BootKeyboard.write(KEYPAD_9);
+      BootKeyboard.write(KEYPAD_5);
+      BootKeyboard.write(KEYPAD_5);   
       break;
       case 8: // Greek letter Delta
-      Serial.println("F3");
-      Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.write(KP_9);
-      Keyboard.write(KP_4);
-      Keyboard.write(KP_8);   
+      // Serial.println("F3");
+      BootKeyboard.press(KEY_LEFT_ALT);
+      BootKeyboard.write(KEYPAD_9);
+      BootKeyboard.write(KEYPAD_4);
+      BootKeyboard.write(KEYPAD_8);   
       break;
     }
     break;
     case 8:
     switch(current_output){
       case 2:
-      Serial.println("C2");
-      Keyboard.press(KEY_VOLUMEDOWN);
+      // Serial.println("C2");
+      Consumer.write(MEDIA_VOL_DOWN);
+      return; // Returns to avoid the delay()
       /*
       Keyboard.press(KEY_LEFT_GUI);
       Keyboard.press('r');
@@ -246,120 +231,126 @@ void printKeyPressed(int input_registered, int current_output){
       Keyboard.println("notepad++.exe");   */   
       break;
       case 5:
-      Serial.println("D8");
-      Keyboard.press(KP_PERIOD);
+      // Serial.println("D8");
+      BootKeyboard.press(KEYPAD_DOT);
       break;
       case 6:
-      Serial.println("D7");
-      Keyboard.press(KP_3);
+      // Serial.println("D7");
+      BootKeyboard.press(KEYPAD_3);
       break;
       case 7:
-      Serial.println("D6");
-      Keyboard.press(KP_6);
+      // Serial.println("D6");
+      BootKeyboard.press(KEYPAD_6);
       break;
       case 8:
-      Serial.println("D5");
-      Keyboard.press(KP_9);
+      // Serial.println("D5");
+      BootKeyboard.press(KEYPAD_9);
       break;
       case 10:
-      Serial.println("D2");
-      Keyboard.press(KEY_VOLUMEUP);
+      // Serial.println("D2");
+      Consumer.write(MEDIA_VOL_UP);
+      return; // Returns to avoid the delay()
       break;
     }
     break;
     case 9:
     switch(current_output){
       case 2: // Volume mute
-      Serial.println("B2");
-      Keyboard.press(KEY_MUTE);
+      // Serial.println("B2");
+      Consumer.write(MEDIA_VOLUME_MUTE);
       break;
       case 5:
-      Serial.println("C8");
-      Keyboard.print("00");
+      // Serial.println("C8");
+      BootKeyboard.print("00");
       break;
       case 6:
-      Serial.println("C7");
-      Keyboard.press(KP_2);
+      // Serial.println("C7");
+      BootKeyboard.press(KEYPAD_2);
       break;
       case 7:
-      Serial.println("C6");
-      Keyboard.press(KP_5);
+      // Serial.println("C6");
+      BootKeyboard.press(KEYPAD_5);
       break;
       case 8:
-      Serial.println("C5");
-      Keyboard.press(KP_8);
+      // Serial.println("C5");
+      BootKeyboard.press(KEYPAD_8);
       break;
-      case 9: // Open my website
-      Serial.println("G3");     
+      case 9: // Play a song!
+      // Serial.println("G3");     
+      playRickRoll(PIN_TONE);
+      
+      /*
+      // Open my website
       Keyboard.press(KEY_LEFT_GUI);
       Keyboard.press('r');
       delay(100);
       Keyboard.releaseAll();
       Keyboard.println("https://www.brycedombrowski.com/");
+      */
       break;
     }
     break;
     case 10:
     switch(current_output){
       case 1: // Greek letter Beta
-      Serial.println("F2");
-      Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.write(KP_9);
-      Keyboard.write(KP_4);
-      Keyboard.write(KP_6);   
+      // Serial.println("F2");
+      BootKeyboard.press(KEY_LEFT_ALT);
+      BootKeyboard.write(KEYPAD_9);
+      BootKeyboard.write(KEYPAD_4);
+      BootKeyboard.write(KEYPAD_6);   
       break;
       case 2: // Greek letter Alpha
-      Serial.println("E2");
-      Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.write(KP_9);
-      Keyboard.write(KP_4);
-      Keyboard.write(KP_5);     
+      // Serial.println("E2");
+      BootKeyboard.press(KEY_LEFT_ALT);
+      BootKeyboard.write(KEYPAD_9);
+      BootKeyboard.write(KEYPAD_4);
+      BootKeyboard.write(KEYPAD_5);     
       break;
       case 5:
-      Serial.println("B8");
-      Keyboard.press(KP_0);
+      // Serial.println("B8");
+      BootKeyboard.press(KEYPAD_0);
       break;
       case 6:
-      Serial.println("B7");
-      Keyboard.press(KP_1);
+      // Serial.println("B7");
+      BootKeyboard.press(KEYPAD_1);
       break;
       case 7:
-      Serial.println("B6");
-      Keyboard.press(KP_4);
+      // Serial.println("B6");
+      BootKeyboard.press(KEYPAD_4);
       break;
       case 8:
-      Serial.println("B5");
-      Keyboard.press(KP_7);
+      // Serial.println("B5");
+      BootKeyboard.press(KEYPAD_7);
       break;
       case 10:
-      Serial.println("B3");
+      // Serial.println("B3");
       break;
     }
     break;
     case 11:
     switch(current_output){
       case 3: // Open new tab in internet browser
-      Serial.println("B1");
-      Keyboard.press(KEY_LEFT_CTRL);
-      Keyboard.press('t');
+      // Serial.println("B1");
+      BootKeyboard.press(KEY_LEFT_CTRL);
+      BootKeyboard.press('t');
       break;
       case 4:
-      Serial.println("A1");
-      Keyboard.press(KEY_LEFT_GUI);
-      Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.press('d');
+      // Serial.println("A1");
+      BootKeyboard.press(KEY_LEFT_GUI);
+      BootKeyboard.press(KEY_LEFT_ALT);
+      BootKeyboard.press('d');
       break;
     }
     break;
   }
 
   delay(50);
-  Keyboard.releaseAll();
+  BootKeyboard.releaseAll();
   delay(150);
 }
 //const int keyPressed[][11] = {{},};
 
-void playTune (){
+/*void playTune (){
   int melody[] = {
     NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
   };
@@ -375,34 +366,43 @@ void playTune (){
     // to calculate the note duration, take one second divided by the note type.
     //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
     int noteDuration = 1000 / noteDurations[thisNote];
-    tone(TONE_PIN, melody[thisNote], noteDuration);
+    tone(PIN_TONE, melody[thisNote], noteDuration);
 
     // to distinguish the notes, set a minimum time between them.
     // the note's duration + 30% seems to work well:
     int pauseBetweenNotes = noteDuration * 1.30;
     delay(pauseBetweenNotes);
     // stop the tone playing:
-    noTone(TONE_PIN);
+    noTone(PIN_TONE);
   
   }
+}*/
+
+void forceNumLock(){
+  // Forces NumLock to stay on. The ALT keycodes used by the keypad buttons wouldn't work otherwise.
+  if (!(BootKeyboard.getLeds() & LED_NUM_LOCK))
+    BootKeyboard.write(KEY_NUM_LOCK);  
 }
 
+/*
 void displayLCD(){
   U8GLIB_SSD1306_128X32 u8g(U8G_I2C_OPT_NONE); // for 0.91" model
   u8g.setFont(u8g_font_unifont);
   u8g.setPrintPos(0,10);
   u8g.print("Hello World!";
-}
+}*/
 
 void setup() {
   // This code runs once
-  Keyboard.begin(); // Set up keyboard
+  
+  BootKeyboard.begin();
+  Consumer.begin();
   pinMode(SHIFT_DATA, OUTPUT);
   pinMode(SHIFT_LATCH, OUTPUT);
   pinMode(SHIFT_CLK, OUTPUT);
   pinMode(OUTPUT_1, OUTPUT);
   pinMode(OUTPUT_2, OUTPUT);
-  pinMode(TONE_PIN, OUTPUT);
+  pinMode(PIN_TONE, OUTPUT);
   
   int pin;
   for (pin = INPUT_FIRST; pin <= INPUT_LAST; pin++){
@@ -412,12 +412,14 @@ void setup() {
   //set shift register to 0
   shiftOut(SHIFT_DATA, SHIFT_CLK, MSBFIRST, 0);
 
-  displayLCD();
+  //displayLCD(); 
 }
 
 void loop() {
   // This code runs repeatedly
-
+  forceNumLock();
+  
+  
   int output_pin;
   for (output_pin = 1; output_pin <= 10; output_pin++){
     // Only one output should be HIGH at a time.
@@ -458,7 +460,7 @@ void loop() {
           //Serial.println(input_pin);
           //Serial.println(String(input_pin-INPUT_FIRST));
           //Serial.println("INPUT: I"+(input_pin+12-INPUT_FIRST >= 14 ? String(input_pin + 14-INPUT_FIRST) : String(input_pin + 12-INPUT_FIRST)));
-          printKeyPressed(input_pin-INPUT_FIRST, output_pin);
+          keyPressed(input_pin-INPUT_FIRST, output_pin);
         }
       }
     }
